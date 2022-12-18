5be4753e1e3187359a4a0093afa32d39766dc158
lindalin999 <102980822+lindalin999@users.noreply.github.com>
2022-12-19 00:30:28 +0800

    Update pomodoro1.cpp

--- pomodoro1.cpp
+++ pomodoro1.cpp
@@ -3,31 +3,45 @@
 #include <windows.h>
 using namespace std;
 int main(){
-	float t,ff=0,w;							//宣告t番茄鐘個數、ff完成件數、w工作件數 
+	
+	float t=0,w;							//t番茄鐘個數 w工作件數 
 	float studytime,resttime;				//宣告每個番茄鐘的工作時長、休息時長
-	float total,finish[10]; 				//total完成%數、每個工作進度%
-	string content[10];						//欲完成工作內容 
-	int timee[10],im[10];
+	float total; 							//total最後完成%數
+	string content[10];						//content欲完成工作內容 
+	int im[10];								//im花費番茄鐘個數
 	
 	cout<<"1.設定番茄鐘"<<endl;
-	cout<<"請依序設定番茄鐘個數、每個番茄鐘的工作時長、休息時長："<<endl;
-	cin>>t>>studytime>>resttime;
-	int ssec=studytime*60; 				//轉換成秒數，方便後面計時器運行 
+	cout<<"請設定每個番茄鐘的工作時長、休息時長："<<endl;
+	cin>>studytime>>resttime;
+	int ssec=studytime*60; 					//轉換成秒數，方便後面計時器運行 
 	int rsec=resttime*60;
 	cout<<endl;
 	
 	cout<<"2.To-do list"<<endl;
 	cout<<"請輸入工作件數： "; 
 	cin>>w;
-	cout<<"請依編號輸入工作內容、預計花費番茄鐘個數，並輸入重要程度(1-5)："<<endl;
+	cout<<"請依編號輸入工作內容、預計花費番茄鐘個數："<<endl;
 	for(int i=0;i<w;i++){
-		cout<<"task "<<i+1<<":";		//事先將每項工作編號，方便後續操作 
-		cin>>content[i]>>timee[i]>>im[i];
+		cout<<"task "<<i+1<<":";			
+		cin>>content[i]>>im[i];
+		t=t+im[i];
 	} 
 	cout<<endl;
-
-	 
-	cout<<"----計時開始請按下鍵盤上的's'----"<<endl;
+	
+	for(int i=0;i<w;i++){					//泡沫排序法(由時長小到大排先後順序) 
+		for(int j=i+1;j<w;j++){
+			if(im[i]>im[j]){
+				swap(content[i],content[j]);
+			}
+		}
+	}
+	cout<<"建議順序";
+	for(int i=0;i<w;i++){
+		cout<<"List "<<i+1<<" "<<content[i]<<endl;
+	}
+	cout<<endl;
+	
+	cout<<"----計時開始請按下鍵盤上的's'----"<<endl<<endl;
 	if(getch()=='s'){
 		for(int k=0;k<t;k++){						//k紀錄目前到第幾個番茄鐘
 			/*工作時間*/ 
@@ -36,12 +50,6 @@ int main(){
         		cout << string(100,'\b');           //退格，以免重複印出 
         		Sleep(1000);            			//延遲一秒--每個倒數時間間隔一秒 
     		}
-    		cout<<"請問完成工作件數、編號及進度(%)"<<endl;
-    		cin>>a;
-    		for(int i=0;i<a;i++){
-    			cin>>b; 
-			}
-			cout<<endl;
 
     		/*休息時間*/ 
     		for(int i=rsec;i>=0;i--){
@@ -49,7 +57,7 @@ int main(){
         		cout << string(100,'\b');          
         		Sleep(1000);            		
     		}			
-			cout<<endl;							//一輪循環結束--繼續下一個循環 
+			cout<<endl;								//一輪循環結束--繼續下一個循環 
 		}
 		
 		cout<<endl;
@@ -59,11 +67,13 @@ int main(){
 	}
 	cout<<endl; 
 
+	int y=0;
 	for(int i=0;i<w;i++){
-		total+=finish[i];
-		total=total/w;
+		cout<<"List "<<i+1<<"："<<content[i]<<" 是否完成?(請用y/n回答)"<<endl;
+		if(getch()=='y') y++;
+		else if(getch()=='n') y+=0;
 	} 
-	cout<<"完成進度："<<total<<endl; 
+	cout<<endl<<"完成進度："<<y/w<<endl; 
     return 0;
 }
 
