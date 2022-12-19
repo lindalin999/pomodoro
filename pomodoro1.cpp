3db98831572866d91f371e8840f41951d7931c94
lindalin999 <102980822+lindalin999@users.noreply.github.com>
2022-12-19 22:10:32 +0800

    Update pomodoro1.cpp

--- pomodoro1.cpp
+++ pomodoro1.cpp
@@ -35,9 +35,9 @@ int main(){
 			}
 		}
 	}
-	cout<<"建議順序";
+	cout<<"建議順序"<<endl;
 	for(int i=0;i<w;i++){
-		cout<<"List "<<i+1<<" "<<content[i]<<endl;
+		cout<<"List "<<i+1<<"："<<content[i]<<endl;
 	}
 	cout<<endl;
 	
@@ -48,14 +48,14 @@ int main(){
 			for(int i=ssec;i>=0;i--){
         		cout <<"Pomodoro "<<k+1<<"--剩餘時間："<<setw(3)<<i/60<<"："<<setw(2)<<i%60;		//set函式限制計時器顯示位數 
         		cout << string(100,'\b');           //退格，以免重複印出 
-        		Sleep(1000);            			//延遲一秒--每個倒數時間間隔一秒 
+        		Sleep(1);            			//延遲一秒--每個倒數時間間隔一秒 
     		}
 
     		/*休息時間*/ 
     		for(int i=rsec;i>=0;i--){
         		cout <<"Rest time "<<k+1<<"--剩餘時間："<<setw(3)<<i/60<<"："<<setw(2)<<i%60;
         		cout << string(100,'\b');          
-        		Sleep(1000);            		
+        		Sleep(1);            		
     		}			
 			cout<<endl;								//一輪循環結束--繼續下一個循環 
 		}
@@ -73,7 +73,7 @@ int main(){
 		if(getch()=='y') y++;
 		else if(getch()=='n') y+=0;
 	} 
-	cout<<endl<<"完成進度："<<y/w<<endl; 
+	cout<<endl<<"完成進度："<<setw(4)<<(y/w)*100<<"%"<<endl;
     return 0;
 }
 
